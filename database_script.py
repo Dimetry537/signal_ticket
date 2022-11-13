c0a75d074941f36f15ed842bc0a4b77aad00a3fd
Дмитрий Максаков <maksakov1988@gmail.com>
2022-11-13 22:07:14 +0300

    add sql with sql alchemy orm

--- database_script.py
+++ database_script.py
@@ -3,6 +3,7 @@ import os
 
 load_dotenv('.env')
 from sqlalchemy import create_engine
+from sqlalchemy.sql import func
 from sqlalchemy.orm import Session
 
 from models import Ticket
@@ -19,7 +20,11 @@ with Session(engine) as session:
    # перенести все SQL в код
     spec = 'Urolog'
     tickets = session.query(Ticket).join(Ticket.doctor).filter(Doctor.specialization == 'Urolog').all()
-    # filter_by, order
+    # SELECT * FROM tickets JOIN doctors ON doctor_id = doctors.id WHERE "doctors"."specialization" = 'Urolog';
+    doctors_ticket = session.query(Ticket, Doctor.name).join(Ticket.doctor).group_by(Doctor.id).count(Ticket.doctor_id)
+    # SELECT COUNT(doctor_id) as count, doctors.name FROM tickets JOIN doctors ON doctor_id = doctors.id GROUP BY "doctors"."id";
+    doctors_3_tickets = session.query(Ticket, Doctor.name).join(Ticket.doctor).group_by(Doctor.id).having(func.count(Ticket.doctor_id) > 3)
+    # SELECT COUNT(doctor_id) as count, doctors.name FROM tickets JOIN doctors ON doctor_id = doctors.id GROUP BY "doctors"."id" HAVING tickets.count > 3;
     # найти все тикеты конкретного доктора
     # напечатать их названия и Id
     # SQL
